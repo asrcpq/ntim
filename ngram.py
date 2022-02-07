e7e378845e04611c7265a3061822990800d8563f
asrcpq <asrcpq@outlook.com>
2022-02-07 09:01:45 +0000

    quick fix

--- ngram.py
+++ ngram.py
@@ -6,7 +6,7 @@ from ntim_data import NtimData
 def dict_filter(d: dict, min_keep):
 	d2 = {}
 	for (key, value) in d.items():
-		if value[1] >= min_keep:
+		if len(value[1]) >= min_keep:
 			d2[key] = value
 	return d2
 
@@ -31,14 +31,20 @@ for n in range(1, 5):
 			continue
 		for i in range(len(line) - n + 1):
 			key = line[i:i+n]
+			if n > 1 and key[:-1] not in ngrams[-1]:
+				continue
+			if i == 0:
+				prev = ''
+			else:
+				prev = line[i - 1]
 			if key in ngram:
-				ngram[key][1] += 1
+				ngram[key][1].add(prev)
 			else:
 				ks = keyseq(key)
-				ngram[key] = [ks, 1]
+				ngram[key] = [ks, {prev}]
 	if n == 1:
 		ntim_data.unigram_sum = sum([
-			x[1] for x in ngram.values()
+			len(x[1]) for x in ngram.values()
 		])
 	elif n == 2:
 		ntim_data.bigram_n1 = sum([
@@ -56,7 +62,13 @@ for n in range(1, 5):
 		for i in range(len(ngrams)):
 			ngrams[i] = dict_filter(ngrams[i], min_keep)
 
-ntim_data.ngrams = ngrams
+ngrams_true = []
+for ngram in ngrams:
+	ngrams_true.append({
+		x: (lat, len(prev_set)) for x, (lat, prev_set) in ngram.items()
+	})
+
+ntim_data.ngrams = ngrams_true
 
 with open('ntim.pkl', 'wb') as file:
 	pickle.dump(ntim_data, file)
